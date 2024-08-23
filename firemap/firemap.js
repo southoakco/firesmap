73ba4e935f5440a884711ca20bfae0673d5c487c
southoakco <southoakco@gmail.com>
2024-08-22 21:53:04 -0500

    Add files via upload

---
+++ firemap/firemap.js
@@ -0,0 +1,4 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
